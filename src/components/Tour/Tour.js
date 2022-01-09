3ba3198897e704932afb1be9076a4dd8b751ea15
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-01-10 02:59:29 +0600

    slider change

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import { Button, Card, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './Tour.css'
+import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Tour.css";
 
 const Tour = (props) => {
-    const { _id, name, description, price, img, duration } = props.tour;
+  const { _id, name, description, price, img, duration } = props.tour;
 
-
-    return (
-        <Col>
-            <Card style={{ marginBottom: "40px" }}>
-                <Card.Img variant="top" className="p-3" height="200px" src={img} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <span className="d-flex">
-                        <p className="duration">{duration}</p>
-                        <p className="ms-auto price">$ {price}</p>
-                    </span>
-                    <Link to={`/tours/tourDetails/${_id}`}>
-                        <button className="btn-book">Book Now</button>
-                    </Link>
-                </Card.Body>
-            </Card>
-        </Col>
-    );
+  return (
+    <Col>
+      <div className="tour-card" style={{ marginBottom: "40px" }}>
+        <div className="card-image">
+          <Card.Img variant="top" className="p-3 " height="200px" src={img} />
+        </div>
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <span className="d-flex">
+            <p className="duration">{duration}</p>
+            <p className="ms-auto price">$ {price}</p>
+          </span>
+          <Link to={`/tours/tourDetails/${_id}`}>
+            <button className="btn-book">Book Now</button>
+          </Link>
+        </Card.Body>
+      </div>
+    </Col>
+  );
 };
 
 export default Tour;
